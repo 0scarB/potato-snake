5ca2a883737ba855f45a503266eb281bd16153c4
0scarB <oscarb@protonmail.com>
2024-09-15 15:36:12 +0200

    Segment lines into groups in browser DOM to improve performance

--- index.js
+++ index.js
@@ -12,12 +12,49 @@ window.onload = () => {
     const monospace_text_advance_y = text_size_reference_el.clientHeight/4;
     const terminal_width  = Math.floor(terminal_el.clientWidth /monospace_text_advance_x);
     const terminal_height = Math.floor(terminal_el.clientHeight/monospace_text_advance_y);
-    let terminal_str = "";
+
+    const line_len = terminal_width + 1;
+    const target_line_group_len = 1024;
+    const lines_per_line_group = Math.ceil(target_line_group_len/line_len);
+    const blank_line            = ' '.repeat(terminal_width) + '\n';
+    const blank_line_group_text = blank_line.repeat(lines_per_line_group);
+    const line_groups_texts      = [];
+    const line_groups_text_nodes = [];
+    for (let line = 0; line < terminal_height; line += lines_per_line_group) {
+        const line_group_text = blank_line_group_text;
+
+        line_groups_texts.push(line_group_text);
+
+        const line_group_el = document.createElement("span");
+        line_group_el.style.position = "absolute";
+        line_group_el.style.display  = "block";
+        line_group_el.style.top      = line*monospace_text_advance_y + "px";
+        line_group_el.style.left     = "0";
+        line_group_el.innerHTML      = line_group_text;
+        terminal_el.appendChild(line_group_el);
+
+        line_groups_text_nodes.push(line_group_el.childNodes[0]);
+    }
+    const line_groups_need_update_idxs = [];
 
     let terminal_cursor_x = 0;
     let terminal_cursor_y = 0;
-    const terminal_str_idx = () =>
-        terminal_cursor_y*(terminal_width + 1) + terminal_cursor_x;
+
+    const terminal_write = (str) => {
+        const line_offset    =  terminal_cursor_y % lines_per_line_group;
+        const line_group_idx = (terminal_cursor_y - line_offset)/lines_per_line_group;
+        const start_idx      = line_offset*line_len + terminal_cursor_x;
+
+        const old_text = line_groups_texts[line_group_idx];
+        line_groups_texts[line_group_idx] =
+            old_text.slice(0, start_idx) + str + old_text.slice(start_idx + str.length);
+
+        if (line_groups_need_update_idxs.indexOf(line_group_idx) == -1) {
+            line_groups_need_update_idxs.push(line_group_idx);
+        }
+
+        terminal_cursor_x += str.length;
+    }
 
     const utf8_text_decoder = new TextDecoder();
 
@@ -73,39 +110,29 @@ window.onload = () => {
             wasm_terminal_write(str_ptr, str_len) {
                 const str = utf8_text_decoder.decode(
                     new Uint8Array(wasm_mem.buffer, str_ptr, str_len));
-
-                const old_terminal_str = terminal_str;
-                terminal_str = terminal_str.slice(0, terminal_str_idx());
-                terminal_str += str;
-                terminal_cursor_x += str.length;
-                terminal_str += old_terminal_str.slice(terminal_str_idx());
+                terminal_write(str);
             },
             wasm_terminal_write_int(x) {
-                const str = x.toString();
-
-                const old_terminal_str = terminal_str;
-                terminal_str = terminal_str.slice(0, terminal_str_idx());
-                terminal_str += str;
-                terminal_cursor_x += str.length;
-                terminal_str += old_terminal_str.slice(terminal_str_idx());
+                terminal_write(x.toString());
             },
             wasm_terminal_move_cursor(x, y) {
                 terminal_cursor_x = x;
                 terminal_cursor_y = y;
             },
             wasm_terminal_flush_out() {
-                terminal_el.innerHTML = terminal_str;
+                for (const idx of line_groups_need_update_idxs) {
+                    line_groups_text_nodes[idx].nodeValue = line_groups_texts[idx];
+                }
+                line_groups_need_update_idxs.length = 0;
             },
             wasm_terminal_clear() {
-                terminal_str = "";
-                for (let y = 0; y < terminal_height; ++y) {
-                    for (let x = 0; x < terminal_width; ++x) {
-                        terminal_str += " ";
+                for (let idx = 0; idx < line_groups_texts.length; ++idx) {
+                    line_groups_texts[idx] = blank_line_group_text;
+
+                    if (line_groups_need_update_idxs.indexOf(idx) == -1) {
+                        line_groups_need_update_idxs.push(idx);
                     }
-                    terminal_str += "\n";
                 }
-                terminal_cursor_x = 0;
-                terminal_cursor_y = 0;
             }
         }},
     ).then(result => {
@@ -114,7 +141,7 @@ window.onload = () => {
 
         wasm_dyn_mem_start = wasm_mem.grow(1)*WASM_MEM_PAGE_SIZE;
         wasm_dyn_mem_ptr   = wasm_dyn_mem_start;
-        wasm_dyn_mem_stop  = wasm_dyn_mem_start*WASM_MEM_PAGE_SIZE;
+        wasm_dyn_mem_stop  = wasm_dyn_mem_start;
 
         const update = wasm_inst.exports.update;
         let t0 = performance.now()/1000;
